4a88ba234d32c445da3f11192efaf1e5008cc500
Jason Saelhof <jsaelhof@gmail.com>
2024-05-11 21:46:47 -0600

    Update insert-song to use the proper db location and to allow comma separated list of values

--- insert-song.mjs
+++ insert-song.mjs
@@ -1,28 +1,30 @@
 import { readFileSync, writeFileSync } from "fs";
 
-const DB = "./packages/server/public/db.json";
+const DB = "./packages/server/db/db.json";
 
 if (process.argv.length > 2) {
-  const path = process.argv[2];
+  const paths = process.argv[2].split(",");
 
-  if (path.startsWith("/Volumes/Public/Music/")) {
-    const db = readFileSync(DB, {
-      encoding: "utf-8",
-    });
-    const dbjson = JSON.parse(db);
-    const lastKey = Object.keys(dbjson).at(-1);
+  paths.forEach((path) => {
+    if (path.startsWith("/Volumes/Public/Music/")) {
+      const db = readFileSync(DB, {
+        encoding: "utf-8",
+      });
+      const dbjson = JSON.parse(db);
+      const lastKey = Object.keys(dbjson).at(-1);
 
-    if (lastKey) {
-      const nextKey = (parseInt(lastKey) + 1).toString().padStart(4, "0");
-      dbjson[nextKey] = {
-        file: path.replace("/Volumes/Public/Music/", ""),
-      };
+      if (lastKey) {
+        const nextKey = (parseInt(lastKey) + 1).toString().padStart(4, "0");
+        dbjson[nextKey] = {
+          file: path.replace("/Volumes/Public/Music/", ""),
+        };
 
-      writeFileSync(DB, JSON.stringify(dbjson, null, 2), "utf8");
+        writeFileSync(DB, JSON.stringify(dbjson, null, 2), "utf8");
+      }
+    } else {
+      console.log("Only files from /Volumes/Public/Music/ are allowed");
     }
-  } else {
-    console.log("Only files from /Volumes/Public/Music/ are allowed");
-  }
+  });
 } else {
   console.log("No file path");
 }
